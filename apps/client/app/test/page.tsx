1d35dc2da477c99dab68d3fde9306d9397ac051e
MAG1980 <76469986+MAG1980@users.noreply.github.com>
2024-01-09 16:23:47 +0300

    Протестировано получение данных от сервера на клиенте.

---
+++ apps/client/app/test/page.tsx
@@ -0,0 +1,19 @@
+'use client'
+import { useEffect, useState } from "react";
+
+export default function () {
+  const [data, setData] = useState('initState');
+  useEffect(() => {
+    fetch('/api')
+      .then(res => res.text())
+      .then(response => setData(response))
+      .catch(error => console.log(error))
+
+    return () => {
+    };
+  }, []);
+
+  return (
+    <div>{ data }</div>
+  )
+}
