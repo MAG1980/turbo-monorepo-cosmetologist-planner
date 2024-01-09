302bd95bc12e70665d4c1bd671a8fbed741fe6f7
MAG1980 <76469986+MAG1980@users.noreply.github.com>
2024-01-09 13:23:59 +0300

    Настроено перенаправление запросов к API в next.config.js. Настроена загрузка глобальных переменных окружения из .env.

--- apps/client/next.config.js
+++ apps/client/next.config.js
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+
+const apiUrl = `http://${ process.env.HOST }:${ process.env.API_PORT }`
+
+const nextConfig = {
+	async rewrites() {
+		return [
+			{
+				source: '/api',
+				destination: apiUrl,
+			},
+			{
+				source: '/api/:path',
+				destination: `${ apiUrl }/:path`,
+			}
+		]
+	}
+}
 
 module.exports = nextConfig
