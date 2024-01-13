e79d2edb6c165029f25d95b95ccb2299bd865f96
MAG1980 <76469986+MAG1980@users.noreply.github.com>
2024-01-13 09:38:21 +0300

    Внутри сервера установлены пакеты tRPC-сервера Zod. В AppModule подключен TrpcModule.

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TrpcModule } from "@server/trpc/trpc.module";
+import { ConfigModule } from "@nestjs/config";
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot(),
+    TrpcModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+}
