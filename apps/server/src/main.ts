5a6efd0b2c5c75c59e0a65dece5ab42c38063fb5
MAG1980 <al-1980@inbox.ru>
2024-04-24 23:46:45 +0300

    Подключён cli-плагин @nestjs/swagger для автоматической генерации документации.

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,12 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { TrpcRouter } from '@server/trpc/trpc.router';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   const trpcRouter = app.get(TrpcRouter);
   await trpcRouter.applyMiddleware(app);
+
+  const config = new DocumentBuilder()
+    .setTitle('Cosmetologist planner')
+    .setDescription('API for making an appointment with a cosmetologist')
+    .setVersion('1.0')
+    .addTag('receptions')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(5000);
 }
 bootstrap();
