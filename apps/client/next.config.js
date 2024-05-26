a833e269f058909d8725b23a3aee17a93375e121
MAG1980 <al-1980@inbox.ru>
2024-05-26 08:55:33 +0300

    Избавился от дублирования переменных окружения в NextConfig.

--- apps/client/next.config.js
+++ apps/client/next.config.js
@@ -1,20 +1,20 @@
 /** @type {import('next').NextConfig} */
 
-const apiUrl = `http://${ process.env.HOST }:${ process.env.API_PORT }`
+const apiUrl = `${process.env.APP_PROTOCOL}://${process.env.APP_HOST}:${process.env.APP_PORT}`;
 
 const nextConfig = {
-	async rewrites() {
-		return [
-			{
-				source: '/api',
-				destination: apiUrl,
-			},
-			{
-				source: '/api/:path',
-				destination: `${ apiUrl }/:path`,
-			}
-		]
-	}
-}
+  async rewrites() {
+    return [
+      {
+        source: "/api",
+        destination: apiUrl,
+      },
+      {
+        source: "/api/:path",
+        destination: `${apiUrl}/:path`,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
