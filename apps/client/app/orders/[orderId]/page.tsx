07148b3c029c719fde9f394d0d9e98cbceaeefc1
MAG1980 <al-1980@inbox.ru>
2024-06-03 00:45:36 +0300

    Созданы кастомные страницы not-found для всего приложения и для динамического маршрута (использована функция notFound()).

--- apps/client/app/orders/[orderId]/page.tsx
+++ apps/client/app/orders/[orderId]/page.tsx
@@ -1,14 +1,16 @@
 import { API_ORDERS_URL } from "@client/common/constants";
 import { log } from "next/dist/server/typescript/utils";
+import { notFound } from "next/navigation";
 
 export default async function OrderDetails({
   params,
 }: {
-  params: { prouductId: string };
+  params: { orderId: string };
 }) {
-  const order = await fetch(`${API_ORDERS_URL}/${params.prouductId}`)
+  const order = await fetch(`${API_ORDERS_URL}/${params.orderId}`)
     .then((data) => data.json())
     .catch((error) => log(error));
+  if (!order.id) return notFound();
   return (
     <div>
       OrderDetails:
