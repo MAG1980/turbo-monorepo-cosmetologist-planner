4310e3bbbca1397ad63d3dcd4a595363eea979bd
MAG1980 <76469986+MAG1980@users.noreply.github.com>
2024-06-03 22:05:44 +0300

    Настроено выделение активных ссылок в меню.

--- apps/client/app/(auth)/(with-auth-layout)/layout.tsx
+++ apps/client/app/(auth)/(with-auth-layout)/layout.tsx
@@ -1,20 +1,51 @@
+"use client";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+const navLinks = [
+  { name: "Sign up", href: "/sign-up" },
+  { name: "Sign in", href: "/sign-in" },
+  { name: "Forgot password", href: "/forgot-password" },
+  { name: "Reset password", href: "/reset-password" },
+];
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const pathname = usePathname();
   return (
-    <section>
-      <h2
-        style={{
-          color: "red",
-          backgroundColor: "whitesmoke",
-          textAlign: "center",
-        }}
-      >
-        Auth Nested Layout
-      </h2>
-      {children}
-    </section>
+    <>
+      <nav>
+        <ul className="flex justify-around gap-4">
+          {navLinks.map((link) => {
+            const isActive = pathname.startsWith(link.href);
+            return (
+              <li key={link.name}>
+                <Link
+                  href={link.href}
+                  className={isActive ? "font-bold text-blue-500 mr-4" : ""}
+                >
+                  {link.name}
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
+      </nav>
+      <section>
+        <h2
+          style={{
+            color: "red",
+            backgroundColor: "whitesmoke",
+            textAlign: "center",
+          }}
+        >
+          Auth Nested Layout
+        </h2>
+        {children}
+      </section>
+    </>
   );
 }
