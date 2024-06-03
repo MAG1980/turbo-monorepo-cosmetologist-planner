bf0fd6d648dffc84ab916590e5de64e2e6bdba0e
MAG1980 <al-1980@inbox.ru>
2024-06-03 04:54:00 +0300

    Создан пример добавления статических метаданных на страницу /about путём экспорта объекта metadata. Результирующие метаданные страницы формируются путём объединения метаданных макета и страницы. Метаданные макетов с более глубокой вложенностью имеют более высокий приоритет. Метаданные страницы имеют максимальный приоритет.

---
+++ apps/client/app/about/page.tsx
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "About cosmetologist planner",
+  description: "Description cosmetologist planner",
+};
+
+export default function About() {
+  return (
+    <div>
+      <h1>About</h1>
+    </div>
+  );
+}
