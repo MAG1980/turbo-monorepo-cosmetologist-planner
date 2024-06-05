950c2bfa9634b2dbeff6ff6ff85a309a31a1afa6
MAG1980 <al-1980@inbox.ru>
2024-06-06 01:46:18 +0300

    Создан пример использования props reset (function) в ErrorBoundary, с помощью которого можно повторно выполнить действие, вызвавшее ошибку, например, отправить запрос на сервер.

--- apps/client/app/procedures/[procedureId]/error.tsx
+++ apps/client/app/procedures/[procedureId]/error.tsx
@@ -1,13 +1,29 @@
 "use client";
 import { useEffect } from "react";
 
-export default function Error({ error }: { error: Error }) {
+export default function ErrorBoundary({
+  error,
+  reset,
+}: {
+  error: Error;
+  reset: () => void;
+}) {
   useEffect(() => {
     console.error(`Error fetching orders: ${error.message}`);
   }, [error]);
   return (
-    <div className="flex items-center justify-center h-screen">
-      <h1 className="text-2xl text-red-500">Error fetching procedure</h1>
-    </div>
+    <>
+      <div className="flex items-center justify-center h-screen">
+        <div className="flex-col items-center justify-end">
+          <h1 className="text-2xl text-red-500">Error fetching procedure</h1>
+          <button
+            className="block mx-auto bg-red-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
+            onClick={() => reset()}
+          >
+            Try again
+          </button>
+        </div>
+      </div>
+    </>
   );
 }
