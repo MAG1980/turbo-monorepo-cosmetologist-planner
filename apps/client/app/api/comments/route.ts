9adf40a0840cd10819af4c93c4721b096386fc70
MAG1980 <al-1980@inbox.ru>
2024-06-08 23:12:54 +0300

    Создан пример обработчика POST-запроса.

--- apps/client/app/api/comments/route.ts
+++ apps/client/app/api/comments/route.ts
@@ -1,5 +1,21 @@
+import { Comment } from "@client/app/api/comments/data";
 import { comments } from "@client/app/api/comments/data";
 
 export function GET() {
   return Response.json(comments);
+}
+
+export async function POST(request: Request) {
+  const data: { text: string } = await request.json();
+  const comment: Comment = {
+    id: comments.length + 1,
+    text: data.text,
+  };
+  comments.push(comment);
+  return new Response(JSON.stringify(comment), {
+    headers: {
+      "Content-Type": "application/json",
+    },
+    status: 201,
+  });
 }
