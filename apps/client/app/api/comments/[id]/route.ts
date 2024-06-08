c38c2ee38f7c7beb96da73c2199048bfcb6a31f8
MAG1980 <al-1980@inbox.ru>
2024-06-09 00:24:13 +0300

    Создан пример обработчика PATH-запроса.

--- apps/client/app/api/comments/[id]/route.ts
+++ apps/client/app/api/comments/[id]/route.ts
@@ -7,3 +7,28 @@ export function GET(_request: Request, { params }: { params: { id: string } }) {
   );
   return Response.json(comment, { status: 200 });
 }
+
+export async function PATCH(
+  request: Request,
+  { params }: { params: { id: string } },
+) {
+  const body = await request.json();
+  const index = comments.findIndex(
+    (comment) => comment.id === parseInt(params.id),
+  );
+  if (index === -1) {
+    return new Response("Comment not found", { status: 404 });
+  }
+
+  const comment = comments[index];
+  if (!comment) {
+    return new Response("Comment not found", { status: 404 });
+  }
+  comment.text = body.text;
+  return new Response(JSON.stringify(comment), {
+    headers: {
+      "Content-Type": "application/json",
+    },
+    status: 200,
+  });
+}
