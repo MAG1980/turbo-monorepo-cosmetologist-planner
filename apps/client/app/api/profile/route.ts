1332124c6d98977596dbe295303178626aebb0d6
MAG1980 <al-1980@inbox.ru>
2024-06-09 02:32:34 +0300

    Создан второй пример присваивания значения cookie: с помощью функции cookies().

--- apps/client/app/api/profile/route.ts
+++ apps/client/app/api/profile/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest } from "next/server";
-import { headers } from "next/headers";
+import { headers, cookies } from "next/headers";
 
 export function GET(request: NextRequest) {
   const authorizationHeader = request.headers.get("Authorization");
@@ -8,6 +8,12 @@ export function GET(request: NextRequest) {
   console.log({ authorization: headersList.get("authorization") });
   const theme = request.cookies.get("theme");
   console.log({ theme });
+
+  const requestCookies = cookies();
+  requestCookies.set("resultsPerPage", "20");
+  //Также доступны методы has(), delete()...
+  const resultsPerPage = requestCookies.get("resultsPerPage");
+  console.log({ resultsPerPage });
   return new Response("<h1>Profile API Data</h1>", {
     headers: {
       "Content-Type": "text/html",
